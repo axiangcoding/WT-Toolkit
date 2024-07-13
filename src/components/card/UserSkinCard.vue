879b3f9ef4cef460c72dd9c4b9dca9f4878aa562
axiangcoding <axiangcoding@gmail.com>
2024-07-14 00:17:04 +0800

    translate UserSkinCard.vue

--- src/components/card/UserSkinCard.vue
+++ src/components/card/UserSkinCard.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
 import image1 from "@/assets/images/china_ground_newmodificationresearch.png";
 import { invoke } from "@tauri-apps/api";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 
 const props = defineProps<{
   skinMetadata: any;
@@ -20,25 +23,27 @@ async function showSkin(skin_folder_path: string) {
     </v-img>
 
     <v-card-text>
-      <!-- <div>涂装介绍：[无]</div> -->
-      <div>载具标识 {{ props.skinMetadata.vehicle_id }}</div>
       <div>
-        空间占用
+        <strong>
+          {{ t("wt_skins.card.vehicle_id") }}
+        </strong>
+        {{ props.skinMetadata.vehicle_id }}
+      </div>
+      <div>
+        <strong> {{ t("wt_skins.card.space_occupancy") }}</strong>
         {{ (props.skinMetadata.folder_size / 1024 / 1024).toFixed(2) }}MB
       </div>
-      <!-- {{ props.skinMetadata.resources }} -->
     </v-card-text>
 
     <v-card-actions>
       <v-btn
         color="primary"
-        text="查看文件夹"
+        :text="t('wt_skins.card.show_folder')"
         @click="showSkin(props.skinMetadata.full_path)"
       ></v-btn>
-      <!-- <v-btn color="warning" text="备份"></v-btn> -->
       <v-btn
         color="error"
-        text="删除"
+        :text="t('wt_skins.card.delete')"
         @click="$emit('delete-skin', props.skinMetadata)"
       ></v-btn>
     </v-card-actions>
