f5123a6782448408bb83b88a986944f7b7c038d0
amber <amber@macbook.local>
2024-07-29 17:43:11 +1200

    make wt_ext_cli not use color

--- src-tauri/src/commands/wt_ext_cli.rs
+++ src-tauri/src/commands/wt_ext_cli.rs
@@ -8,7 +8,6 @@ pub struct CmdResult {
     stdout: Option<String>,
     stderr: Option<String>,
 }
-
 #[tauri::command]
 pub async fn exec_wt_ext_cli(
     state: tauri::State<'_, WrappedState>,
@@ -16,26 +15,26 @@ pub async fn exec_wt_ext_cli(
 ) -> Result<CmdResult, RetCode> {
     let wt_ext_cli_path = get_wt_ext_cli_path(state)?;
     debug!("args: {:?}", args);
-
     let mut command = tokio::process::Command::new(wt_ext_cli_path);
     command.args(args);
-
+    command.env("FORCE_SET_COLOR", "false");
+    
     #[cfg(target_os = "windows")]
     {
         const CREATE_NO_WINDOW: u32 = 0x08000000;
         command.creation_flags(CREATE_NO_WINDOW);
     }
-
+    
     let output = command.output().await.map_err(|e| {
         error!("Failed to execute command: {}", e);
         RetCode::WTExtCliCommandFailed
     })?;
-
+    
     let stdout = String::from_utf8(output.stdout).unwrap();
     let stderr = String::from_utf8(output.stderr).unwrap();
     debug!("stdout: {}", stdout);
     debug!("stderr: {}", stderr);
-
+    
     Ok(CmdResult {
         code: output.status.code().unwrap(),
         stdout: Some(stdout),
